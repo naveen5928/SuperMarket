896c896bb9983772c1c0f56af31b4b5c78ceee4f
Naveen <naveennaidu7709@gmail.com>
2024-09-30 00:28:34 -0500

    Update About content

--- SuperMarket/src/Components/About.jsx
+++ SuperMarket/src/Components/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
           <p>
             We started operations in 2020. We guarantee fresh produce.
             <br />
-            Save time by shopping on our app and we'll deliver the products
+            Save time by shopping on our app and we will deliver the products
             right to your home. <br />
             <em>We use Stripe to process your payment.</em>
           </p>
